0b16e007bbd142e504ddaea7b0e46da72eee2af7
seraraja <seraraja555@gmail.com>
2023-10-06 11:48:51 +0530

    sera code

--- src/test/java/org/test/Sample.java
+++ src/test/java/org/test/Sample.java
@@ -6,7 +6,9 @@ public class Sample {
 
 	@Test
 	private void test() {
-		System.out.println("test");
+		System.out.println("test1");
+		System.out.println("test2");
+		System.out.println("test3");
 
 	}
 	
