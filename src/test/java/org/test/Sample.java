e4726b3911d9dd44395367706839532e3540301f
seraraja <seraraja555@gmail.com>
2023-10-06 12:03:47 +0530

    raja code

--- src/test/java/org/test/Sample.java
+++ src/test/java/org/test/Sample.java
@@ -6,7 +6,9 @@ public class Sample {
 
 	@Test
 	private void test() {
-		System.out.println("test");
+		System.out.println("test1");
+		System.out.println("test4");
+		System.out.println("test5");
 
 	}
 	
